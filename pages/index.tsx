e8db338a216db264703ac06cd543c5c01f58855b
maureen wang <maureen81711@gmail.com>
2023-02-06 21:56:57 +0800

    feat: add testing button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Link from "next/link";
+import { Button } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={inter.className}>
+        <Button colorScheme="blue">
           <Link href="/about">Go To About Page</Link>
+        </Button>
       </main>
     </>
   );
