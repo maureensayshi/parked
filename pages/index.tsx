07196ef7ba945f79b3613fc31d9c7b153330199e
maureen wang <maureen81711@gmail.com>
2023-02-06 22:47:48 +0800

    feat: add search input field in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Link from "next/link";
-import { Button } from "@chakra-ui/react";
+import { Input, Button, Flex, Container } from "@chakra-ui/react";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+
   return (
     <>
       <main className={inter.className}>
-        <Button colorScheme="blue">
-          <Link href="/about">Go To About Page</Link>
-        </Button>
+        <Container m={4} p={0}>
+          <Flex gap={2}>
+            <Input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Search area ..."
+              focusBorderColor="teal.600"
+              w={60}
+            />
+            <Button colorScheme="teal">Search</Button>
+          </Flex>
+        </Container>
       </main>
     </>
   );
